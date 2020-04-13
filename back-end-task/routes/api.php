10caa413cfecf66d0aff7368fe9537f625c0b5d5
donyino <donyino@fh.org>
2020-04-13 16:24:07 +0300

    Route fix

--- back-end-task/routes/api.php
+++ back-end-task/routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/v1/on-covid-19/{format?}', 'Covid19Controller@getData');
+Route::get('/v1/on-covid-19/{format?}', 'Covid19Controller@getData');
